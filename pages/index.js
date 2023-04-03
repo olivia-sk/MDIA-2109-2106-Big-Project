70053d349237365ce54a56f54fb3bb54b7a15be9
dhfflqldkk <ofong4@my.bcit.ca>
2023-04-03 12:36:21 -0700

    Olivia

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        
       <Menu/>
       </main>
     </>
