0d768bec3c602466ce3f777e1a8e84e0fc59e308
dhfflqldkk <ofong4@my.bcit.ca>
2023-04-12 14:04:39 -0700

    main

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import styles from "./Menu.module.css"
+import styles from "./Menu.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
