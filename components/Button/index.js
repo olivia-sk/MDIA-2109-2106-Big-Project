f8be979af548d969ca639d5c9c78361810a73e64
Sierra Laurent <113314536+sierralaurent@users.noreply.github.com>
2023-04-12 12:37:25 -0700

    Add files via upload

---
+++ components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/components/Button/Button.module.css'
+
+export default function App() {
+  return (
+    <>
+    <div>
+    <Link href="" className={styles.link}><button className={styles.button}>Yes</button></Link>
+    </div>
+    </>
+  )
+}
