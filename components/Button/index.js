1818c90e0956254e309c2cc8bb9680bed89d688f
olivia <113372869+dhfflqldkk@users.noreply.github.com>
2023-04-12 13:03:35 -0700

    Update index.js

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '@/components/Button/Button.module.css'
 
-export default function App() {
+export default function Button() {
   return (
     <>
     <div>
