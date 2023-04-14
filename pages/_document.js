b05875bde474060ee94eb6dd431cd9cf1ef7092b
olivia <113372869+dhfflqldkk@users.noreply.github.com>
2023-04-14 00:19:38 -0700

    linked fonts to _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link href='https://fonts.googleapis.com/css?family=Montserrat'/>
-      <link href='https://fonts.googleapis.com/css?family=Comfortaa'/>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
