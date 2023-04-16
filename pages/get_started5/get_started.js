44580faf24283e1247c21d551f73f40ece37702d
YiLong-Ma <echan241@my.bcit.ca>
2023-04-15 22:18:09 -0700

    get started page

--- pages/get_started5/get_started.js
+++ pages/get_started5/get_started.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import Pie from "@/components/PieChart";
+import styles from "@/styles/GetStarted.module.css";
 import Link from "next/link";
 import Navigation from "@/components/Menu";
-import Button from "@/components/Button";
 import WaterWave from "@/components/WaterWave";
 
 export default function Getstarted() {
@@ -19,9 +17,38 @@ export default function Getstarted() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.parent}>
-          <Navigation/>
-            <WaterWave/>
+      <main className={styles.main}>
+        <Navigation />
+        <div className={styles.container}>
+          <h1 className={styles.title}>New Habits</h1>
+
+          <div className={styles.content_container}>
+            <Image
+              className={styles.image_container}
+              src="/Graphics/flowerwhale.png"
+              alt="whale holding a flower"
+              width={192}
+              height={122}
+            />
+            <p className={styles.description}>
+              This quiz is designed to test if you or your loved one’s
+              may be struggling with substance misuse disorder (SUD).
+              <div className={styles.bottomtext}>
+                To start the quiz, click the button below. Please answer
+                the following questions honestly.
+              </div>
+
+            </p>
+            <div className={styles.button}>
+              <Link href="/statistics3/statistics" className={styles.link}>
+                Let's Go!
+              </Link>
+            </div>
+          </div>
+          <div className={styles.wave}>
+            <WaterWave />
+          </div>
+        </div>
       </main>
     </>
   );
