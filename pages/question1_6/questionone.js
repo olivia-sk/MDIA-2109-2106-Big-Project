e567ab053127dfc560c210f099a312a15428fa57
YiLong-Ma <118211750+YiLong-Ma@users.noreply.github.com>
2023-04-16 15:05:25 -0700

    Update questionone.js

--- pages/question1_6/questionone.js
+++ pages/question1_6/questionone.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import styles from "@/styles/Question1.module.css";
 import Navigation from '@/components/Menu'
 import ProgressBar from '@/components/progressbar'
+import Link from 'next/link';
+import Image from 'next/image';
+import WaterWave from '@/components/WaterWave';
 
 export default function QuestionOne() {
   const currentStep = 1;
@@ -18,10 +21,55 @@ export default function QuestionOne() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.parent}>
+      <main className={styles.main}>
         <Navigation />
-        <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+        <div className={styles.container}>
+          <h1 className={styles.title}>New Habits</h1>
+          <div className={styles.progress}>
+            <ProgressBar currentStep={currentStep} totalSteps={totalSteps} />
+          </div>
+          <div className={styles.content_container}>
+            <p className={styles.description}>
+              Has the substance misuse affected
+              friends and family relationships?
+            </p>
+            <div className={styles.button}>
+              <Link href="#" className={styles.link}>
+                Yes
+              </Link>
+            </div>
+            <div className={styles.button}>
+              <Link href="#" className={styles.link}>
+                No
+              </Link>
+            </div>
+            <div className={styles.image_container}>
+              <Image
+                src="/Graphics/BigCoral3.png"
+                alt="whale holding a flower"
+                width={75}
+                height={68}
+              />
+              <Image
+                src="/Graphics/BigCoral3.png"
+                alt="whale holding a flower"
+                width={75}
+                height={68}
+              />
+            </div>
+
+          </div>
+          <div className={styles.buttonNext}>
+            <Link href="/question2_7/questiontwo" className={styles.link}>
+              Next Question
+            </Link>
+          </div>
+          <div className={styles.wave}>
+            <WaterWave />
+          </div>
+        </div>
       </main>
     </>
   );
 }
+
